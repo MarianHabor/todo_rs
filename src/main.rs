aa8213b01a3caf4bb9869715d622caf014053ee4
Marian <mariohabor@gmail.com>
2023-11-16 19:59:59 +0000

    Higlight menu

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,44 @@ extern crate ncurses;
 
 use ncurses::*;
 
+const REGULAR_PAIR: i16 = 0;
+const HIGHLIGHT_PAIR: i16 = 1;
+
 fn main() {
     initscr();
-
-    addstr("Hello, world!");
-
-    refresh();
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
 
     let mut quit = false;
     let mut todos = vec![
         "Buy bread.",
         "And millk",
         "Make more tea",
-    ]; 
+    ];
 
+    let mut todoCurr: usize = 0;
 
     while !quit {
-        let key = getch();
+        
+        for (index, todo) in todos.iter().enumerate() {
+            let pair = {
+                if todoCurr == index {
+                    HIGHLIGHT_PAIR
+                } else {
+                    REGULAR_PAIR
+                }
+            };
 
-        for (row, todo) in todos.iter().enumerate() {
-            mv(row as i32, 0);
+            attron(COLOR_PAIR(pair));
+            mv(index as i32, 1);
             addstr(*todo);
+            attroff(COLOR_PAIR(pair));
         }
-
+        
+        refresh();
+        
+        let key = getch();
         match key as u8 as char {
             'q' => quit = true,
             _=> {}
