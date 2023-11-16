22183c5c95c79be618aff9dfd3c2cc413f12bab7
Marian <mariohabor@gmail.com>
2023-11-16 19:28:11 +0000

    todos list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate ncurses;
+
+use ncurses::*;
+
 fn main() {
-    println!("Hello, world!");
+    initscr();
+
+    addstr("Hello, world!");
+
+    refresh();
+
+    let mut quit = false;
+    let mut todos = vec![
+        "Buy bread.",
+        "And millk",
+        "Make more tea",
+    ]; 
+
+
+    while !quit {
+        let key = getch();
+
+        for (row, todo) in todos.iter().enumerate() {
+            mv(row as i32, 0);
+            addstr(*todo);
+        }
+
+        match key as u8 as char {
+            'q' => quit = true,
+            _=> {}
+        }
+    }
+
+    endwin();
 }
